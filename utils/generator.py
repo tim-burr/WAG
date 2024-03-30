4d97b26835bc384a574d774be1613f0bed3ac3d2
Timothy Burroughs <president@timbur.tech>
2024-03-30 17:07:37 -0400

    Refactor Frontmatter package implementation
    generator.py:
    Now the filepath is passed into the Frontmatter classmethod, instead of Generator managing the file handle.

--- utils/generator.py
+++ utils/generator.py
@@ -1,41 +1,40 @@
 # Imports
-# Standard
 from pathlib import Path
 # Third Party
-import frontmatter
+from frontmatter import Frontmatter
 from markdown import markdown
 from yattag import indent
-# Local
+# Custom
 import utils.drive_tools as drive
 import utils.config_loader as cfg
+
 
 class Generator:
     def __init__(self, config: cfg.Configuration):
         self._config = config
-    
+
     # ****************
     # Private methods
     # ****************
     def _parse_page(self, page) -> tuple[dict, str]:
         # Split serialized YAML frontmatter from Markdown content
-        with open(page) as f:
-            metadata, content = frontmatter.parse(f.read())
+        metadata, content, *ignore = Frontmatter.read_file(page).values()
         return metadata, content
 
     def _replace_tags(self, doc, tags: dict) -> str:
         for key, value in tags.items():
             doc = doc.replace(key, value)
         return doc
-        
+
     def _md_to_html(self, content) -> str:
         # Convert Markdown into HTML; Markdown inside HTML block tags allowed
         html = markdown(content, extensions=['md_in_html'])
         return html
-    
+
     def _generate_from_md(self, page) -> str:
         metadata, content = self._parse_page(page)
         html_content = self._md_to_html(content)
-        
+
         # Get page template from user configuration path
         template = metadata.get("template")
         html_doc = self._config.templates.find(template)
@@ -59,7 +58,7 @@ class Generator:
                 html_doc = indent(html_doc)
             except:
                 print("Warning: Page could not be indented")
-       
+
         # Determine final path of new page
         build_dir = self._config.paths.get("build")
         page_name = Path(page).stem  # Filename w/o extension
@@ -80,7 +79,7 @@ class Generator:
             # Default: Child page gets their own subfolder
             build_subdir = build_dir / category / page_name
             new_file = build_subdir / "index.html"
-       
+
         # Create directory to store new page, if does not exist
         drive.create_directory(build_subdir)
 
@@ -94,7 +93,7 @@ class Generator:
         # TODO: Implement direct HTML file handling
         html_doc: str
         return html_doc
-    
+
     # ****************
     # Public methods
     # ****************
